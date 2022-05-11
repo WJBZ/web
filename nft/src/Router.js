39fec0b0500823c2cc47d85180fd8f9ed4d09885
WJBZ <wjbz.official@gmail.com>
2022-05-11 15:56:08 +0900

    Change to HashRouter

--- nft/src/Router.js
+++ nft/src/Router.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Header from "./components/header/JC_Header";
 import Footer from "./components/footer/JC_Footer";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Route, Routes } from 'react-router-dom';
 import JannabiClubMint from './pages/WJBZMint';
 // import WhitelistMint from './pages/WJBZWhitelistMint';
 import NotFound from "./pages/NotFound";
@@ -13,7 +13,7 @@ function Router() {
 
   return (
     <> 
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter> {/* basename={process.env.PUBLIC_URL}> */}
       {/* <ScrollTopBehaviour /> */}
       { (window.location.pathname.toLowerCase() !== '/mint')&&(window.location.pathname.toLowerCase() !== '/wlmint') ? <Header/> : null }
       <Routes>
